bb7cfc3458d290ef01694898f535483e1d40797f
LuismallitoDev <cardonaluisma@gmail.com>
2024-12-17 10:18:22 -0500

    12

--- zooAnimales/mamifero.py
+++ zooAnimales/mamifero.py
@@ -3,8 +3,8 @@ class Mamifero(Animal):
     _listado = []
     caballos = 0
     leones = 0
-    def __init__(self, nombre, edad, habitat, genero, zona, pelaje, patas):
-        super().__init__(nombre, edad, habitat, genero, zona)
+    def __init__(self, nombre, edad, habitat, genero, pelaje, patas):
+        super().__init__(nombre, edad, habitat, genero)
         self._pelaje = pelaje
         self._patas = patas
         Mamifero._listado.append(self)
--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -3,8 +3,8 @@ class Pez(Animal):
     _listado = []
     salmones = 0
     bacalaos = 0
-    def __init__(self, nombre, edad, habitat, genero, zona, colorEscamas, cantidadAletas):
-        super().__init__(nombre, edad, habitat, genero, zona)
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas, cantidadAletas):
+        super().__init__(nombre, edad, habitat, genero)
         self._colorEscamas = colorEscamas
         self._cantidadAletas = cantidadAletas
     def movimiento():
